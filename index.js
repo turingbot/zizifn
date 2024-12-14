1ea4ddcdefe3e84e49aa680a10eea5cb7fc49aff
turingbot <tabaghi.m@gmail.com>
2024-12-14 15:50:50 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-// <!--GAMFC-->version base on commit 58686d5d125194d34a1137913b3a64ddcf55872f, time is 2024-11-27 09:26:01 UTC<!--GAMFC-END-->.
-// @ts-ignore
 import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '1b92a69d-6d26-40e2-bdf4-4c574dd9fa77';
 
-let proxyIP = '';
+let proxyIP = '18.193.131.26';
 
 
 if (!isValidUUID(userID)) {
